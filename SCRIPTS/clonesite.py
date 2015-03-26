5206cd6171330899126c8f0cb2a8363d33f14447
Adam Compton <adam.compton@gmail.com>
2015-03-26 09:10:44 -0400

    much code cleanup

--- SCRIPTS/clonesite.py
+++ SCRIPTS/clonesite.py
@@ -4,15 +4,18 @@ import re
 import os
 
 class Cloner(object):
-    def __init__(self, url, path):
+    def __init__(self, url, path, maxdepth=3):
         self.start_url = url
         self.path = os.getcwd() + "/" + path
+        self.maxdepth = maxdepth
+        self.seenurls = []
         self.user_agent="Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)"
 
     # ######################################3
     # Utility Functions
     # ######################################3
     
+    # http get request
     def get_url(self, url):
         headers = { 'User-Agent' : self.user_agent }
         try:
@@ -25,6 +28,7 @@ class Cloner(object):
             else:
                 return ""
 
+    # download a binary file
     def download_binary(self, url):
         filename = ""
         if url.startswith(self.start_url):
@@ -38,6 +42,7 @@ class Cloner(object):
         self.writeoutfile(data, filename)
         return
 
+    # writeout a file
     def writeoutfile(self, data, filename):
         if filename.startswith("/"):
             filename = filename[1:]
@@ -52,6 +57,7 @@ class Cloner(object):
         f.write(data)
         f.close()        
 
+    # unique a list
     def unique_list(self, old_list):
         new_list = []
         if old_list != []:
@@ -63,11 +69,21 @@ class Cloner(object):
     # ######################################3
     # html and link processing functions
     # ######################################3
-    
+
+    # convert all forms to contain hooks
+    def process_form(self, html, method="get", action="index"):
+        return html
+
+    # build new list of only the link types we are interested in
     def process_links(self, links):
         new_links = []
         for link in links:
+            link = link.lower()
             if (link.endswith(".css")  or 
+                link.endswith(".html") or
+                link.endswith(".php")  or
+                link.endswith(".asp")  or
+                link.endswith(".aspx") or
                 link.endswith(".js")   or
                 link.endswith(".ico")  or
                 link.endswith(".png")  or
@@ -75,24 +91,45 @@ class Cloner(object):
                 link.endswith(".jpeg") or
                 link.endswith(".bmp")  or
                 link.endswith(".gif")
+#                ("." not in os.path.basename(link))
                ):
                 new_links.append(link)
         return new_links
     
-    def clone(self, url="", base=""):
+    # primary recersive function used to clone and crawl the site
+    def clone(self, depth=0, url="", base=""):
+        # early out if max depth is reached
+        if (depth > self.maxdepth):
+            print "MAX URL DEPTH       [%s]" % (url)
+            return
+
+        # if no url is specified, then assume the starting url
         if (url == ""):
             url = self.start_url
 
+        # if no base is specified, then assume the starting url
         if (base == ""):
             base = self.start_url
 
+        # check to see if we have processed this url before
+        if (url in self.seenurls):
+            print "ALREADY SEEN URL    [%s]" % (url)
+            return
+        else:
+            self.seenurls.append(url)
+
+        # get the url and return if nothing was returned
         html = self.get_url(url)
         if (html == ""):
             return
 
+        # determine the websites script/filename
         filename = ""
+        # we are only interested in urls on the same site
         if url.startswith(base):
             filename = url[len(base):]
+
+            # if filename is blank, assume index.html
             if (filename == ""):
                 filename = "index.html"
         else:
@@ -112,8 +149,9 @@ class Cloner(object):
 
         # loop over the links
         for link in links:
+            link = link.lower()
             new_link = link
-            if link.lower().startswith("http"):
+            if link.startswith("http"):
                 new_link = link
             elif link.startswith("/"):
                 new_link = base + link
@@ -122,24 +160,38 @@ class Cloner(object):
             else:
                 new_link = base + "/" + link
 
+            good_link = new_link
+            if (new_link.startswith(self.start_url)):
+                good_link = new_link[len(self.start_url):]
+
             print "FOUND A NEW LINK    [%s]" % (new_link)
+            print "FOUND A NEW LINK *  [%s]" % (good_link)
     
             # switch out new_link for link
-            html = html.replace("\"" + link + "\"", "\"" + new_link + "\"")
+            html = html.replace("\"" + link + "\"", "\"" + good_link + "\"")
 
-            # handle "../" properly
-            # recursively call process_html on each non-image link
-
+            # determine is we need to call Clone recursively
             if (link.endswith(".css")  or
+                link.endswith(".html") or
+                link.endswith(".php")  or
+                link.endswith(".asp")  or
+                link.endswith(".aspx") or
                 link.endswith(".js")
+#                ("." not in os.path.basename(link))
                ):
+                # recursively call process_html on each non-image link
                 if base != self.start_url:
-                    self.clone(url=new_link, base=os.path.dirname(url))
+                    self.clone(url=new_link, base=os.path.dirname(url), depth=depth+1)
                 else:
-                    self.clone(url=new_link)
+                    self.clone(url=new_link, depth=depth+1)
             else:
+                # must be a binary file, so just download it
                 self.download_binary(new_link)
-  
+
+        # update any forms within the page  
+        html = self.process_form(html)
+
+        # write out the html for the page we have been processing
         self.writeoutfile(html, filename)
         return
         
